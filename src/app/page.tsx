1415fc296e9ab2fdce60a214a1a0b5a2785e9f4e
sahebbali <saheb1996jmr@gmail.com>
2024-12-19 15:02:55 +0600

    add New Collection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Banner from "@/components/Banner";
 import Featured from "@/components/Featured";
+import NewCollection from "@/components/NewCollection";
 
 const HomePage = () => {
   return (
     <div className="overflow-hidden">
       <Banner />
       <Featured />
+      <NewCollection />
     </div>
   );
 };
