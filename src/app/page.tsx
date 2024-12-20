6985e3a065dbc4f35b3ca5a5f84faea6eeb75cbd
sahebbali <saheb1996jmr@gmail.com>
2024-12-20 21:03:51 +0600

    House Shape Plant

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Banner from "@/components/Banner";
 import Featured from "@/components/Featured";
+import HouseShapePlant from "@/components/HouseShapePlant";
 import NewCollection from "@/components/NewCollection";
 import PopularProducts from "@/components/PopularProducts";
 
@@ -10,6 +11,7 @@ const HomePage = () => {
       <Featured />
       <NewCollection />
       <PopularProducts />
+      <HouseShapePlant />
     </div>
   );
 };
